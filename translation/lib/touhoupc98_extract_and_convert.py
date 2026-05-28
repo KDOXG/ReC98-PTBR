4e8287758fc26c10bf158fdee3266d6d46d59303
KDOXG <kevin.20pereira@gmail.com>
2026-05-28 04:35:45 -0300

    Fix directory in safe case for running game

--- translation/lib/touhoupc98_extract_and_convert.py
+++ translation/lib/touhoupc98_extract_and_convert.py
@@ -150,7 +150,7 @@ def set_and_play_running_game(file_name):
     game_folder = os.path.normpath(os.path.join(export_dir, file_name + "_gamedata"))
     
     if not os.path.exists(game_folder):
-        print(f"Error: Game folder {game_folder} not found. Please extract and convert the game first.")
+        print(f"Error: Game folder {file_name + "_gamedata"} not found. Please extract and convert the game first.")
         return
     
     # Reset running game directory to have the files of only one game
